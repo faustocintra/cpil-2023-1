e818cc6645dc3b34381ad8f3426769b4880b0eba
Fausto G. Cintra <professor@faustocintra.com.br>
2023-03-01 23:31:00 +0000

    (01/03) Processamento dos estados de 0 a 5

--- lexer.js
+++ lexer.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 
 const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
+const blanks = [' ', '\t', '\n', '\r']
+
 /* ABRE O ARQUIVO QUE SERÁ ANALISADO */
 const openFile = () => {
 
@@ -27,22 +29,148 @@ const openFile = () => {
 }
 
 const analyze = source => {
-  let state = 0
+  let state = 0             // Estado do autômato
+  let lexeme = ''           // Lexema sendo lido
+  let char = ''             // Caractere sendo lido
+  const symbolsTable = []   // Tabela de símbolos
+
+  // Função que guarda o caractere atual no lexema
+  // e avança para o próximo estado
+  const advanceTo = nextState => {
+    lexeme += char
+    state = nextState
+  }
+
+  // Acaba de ler um lexema em um estado terminal
+  const terminate = finalState => {
+
+    // Só acrescenta o caractere ao lexema se não for um branco
+    if(! blanks.includes(char)) lexeme += char
+    state = finalState
+    
+    // Insere o lexema na tabela de símbolos,
+    // de acordo com o estado atual
+    switch(state) {
+      case 6.1:   // plus
+        symbolsTable.append({lexeme, token: 'plus'})
+        break
+
+      case 6.2:   // minus
+        symbolsTable.append({lexeme, token: 'minus'})
+        break
+
+      case 6.3:  // times
+        symbolsTable.append({lexeme, token: 'times'})
+        break
+
+      case 6.4:  // div
+        symbolsTable.append({lexeme, token: 'div'})
+        break
+
+      case 6.5:  // lparen
+        symbolsTable.append({lexeme, token: 'lparen'})
+        break
+
+      case 6.6:  // rparen
+        symbolsTable.append({lexeme, token: 'rparen'})
+        break
+
+      case 6.7:  // keyword read
+        symbolsTable.append({lexeme, token: 'keyword', value: lexeme})
+        break
+
+      case 6.8: // identifier
+        symbolsTable.append({lexeme, token: 'identifier', value: lexeme})
+        break
+
+    }
+
+    // Reseta estado e lexema
+    state = 0
+    lexeme = ''
+  }
 
   // Percorre todo o código-fonte, caractere a caractere
   for(let pos = 0; pos < source.length; pos++) {
 
     // Lê um caractere do código-fonte
-    const char = source.charAt(pos)
+    char = source.charAt(pos)
 
     switch(state) {
       case 0:
         
-        if(char === 'r') state = 1
+        if(char === 'r') advanceTo(1)
 
-        else if(char === 'w') state = 7
+        else if(char === 'w') advanceTo(7)
 
-        else if(digits.includes(char)) state = 13
+        else if(digits.includes(char)) advanceTo(13)
+
+        else if(char === '.') advanceTo(15)
+
+        else if(char === ':') advanceTo(17)
+
+        // Qualquer letra, exceto "r" e "w", já processadas acima
+        else if (char.match(/a-zA-Z/)) advanceTo(5)
+
+        else if (char === '+') terminate(6.1)
+
+        else if (char === '-') terminate(6.2)
+
+        else if (char === '*') terminate(6.3)
+
+        else if (char === '/') terminate(6.4)
+
+        else if (char === '(') terminate(6.5)
+
+        else if (char === ')') terminate(6.6)
+
+        // Ignora caracteres em branco
+        else if (blanks.includes(char)) continue
+
+        else console.error(`ERROR: unexpected char ${char} at ${pos}.`)
+
+        break
+
+      case 1:
+
+        if(char === 'e') advanceTo(2)
+        else if(char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else console.error(`ERROR: unexpected char ${char} at ${pos}.`)
+
+        break
+
+      case 2:
+
+        if(char === 'a') advanceTo(3)
+        else if(char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else console.error(`ERROR: unexpected char ${char} at ${pos}.`)
+
+        break
+
+      case 3:
+
+        if(char === 'd') advanceTo(4)
+        else if(char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else console.error(`ERROR: unexpected char ${char} at ${pos}.`)
+
+        break
+
+      case 4:
+
+        if(char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if(blanks.includes(char)) terminate(6.7)
+        else console.error(`ERROR: unexpected char ${char} at ${pos}.`)
+
+        break
+
+      case 5:
+
+        if(char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if(blanks.includes(char)) terminate(6.8)
+        else console.error(`ERROR: unexpected char ${char} at ${pos}.`)
+
+        break
+
     }
   }
 }
